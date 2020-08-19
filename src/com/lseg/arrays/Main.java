f3e1b1c71a551447ab5c86a657b7c1c7bb1e9f2a
LaviniaDinca28 <40751786+LaviniaDinca28@users.noreply.github.com>
2020-08-19 22:59:21 +0300

    Homework arrays

--- src/com/lseg/arrays/Main.java
+++ src/com/lseg/arrays/Main.java
@@ -1,7 +1,24 @@
 package com.lseg.arrays;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Lavinia says hello twice blink blink blink");
-    }
+
+        public static void main (String[]args) {
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter the n and d:");
+            int n = scanner.nextInt(); // n este nr de int din array
+            int d = scanner.nextInt(); // d este nr de rotatii
+            System.out.println("Enter the array:");
+            int[] array = new int[n]; //  este array-ul
+
+            for (int i = 0; i < n; i++) {
+                array[(i + n - d) % n] = scanner.nextInt();
+            }
+            for(int i = 0; i < n; i++) {
+                System.out.print(i + " ");
+            }
+        }
 }
+
