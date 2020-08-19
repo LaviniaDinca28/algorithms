026c3aa83d38c96207991b3788828a06c1b94725
LaviniaDinca28 <40751786+LaviniaDinca28@users.noreply.github.com>
2020-08-19 12:29:01 +0300

    Change2

--- src/com/lseg/arrays/Main.java
+++ src/com/lseg/arrays/Main.java
@@ -2,6 +2,6 @@ package com.lseg.arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Lavinia says hello twice");
+        System.out.println("Lavinia says hello twice blink blink blink");
     }
 }
