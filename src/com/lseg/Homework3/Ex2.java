e2a2bd57c50c1b1a94fd1008dbac1ff62fbd7885
LaviniaDinca28 <40751786+LaviniaDinca28@users.noreply.github.com>
2020-08-30 18:04:34 +0300

    Homework3

--- src/com/lseg/Homework3/Ex2.java
+++ src/com/lseg/Homework3/Ex2.java
@@ -1,31 +1,34 @@
 package com.lseg.Homework3;
 
+import java.util.Scanner;
+
 public class Ex2 {
     public static void main(String[] args) {
 
-        System.out.println(shouldWakeUp(true,-1));
 
+        System.out.println(shouldWakeUp( true,  5));
 
     }
 
-        public static boolean shouldWakeUp(boolean barking, int hour){
+
+    private static boolean shouldWakeUp(boolean barking, int hour){
             boolean result = false;
             if (hour < 0 || hour > 23){
-                System.out.println("1st Condition");
+                System.out.println("Don't wake up");
                 return false;
             }
             else if (hour >= 8 && hour <= 22){
-                System.out.println("2nd Condition");
+                System.out.println("Don't wake up");
                 result = false;
             }
 
             else if (hour >= 1 || hour <=7 || hour >=23){
                 if(barking == true){
-                    System.out.println("3rd Condition");
+                    System.out.println("Wake up");
                     result = true;
                 }
                 else {
-                    System.out.println("4th Condition");
+                    System.out.println("Don't wake up");
                     result = false;
                 }
             }
