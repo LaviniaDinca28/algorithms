baeac2494665a8538a1124ad2081bccde6cd04b7
LaviniaDinca28 <40751786+LaviniaDinca28@users.noreply.github.com>
2020-08-30 16:26:30 +0300

    Homework3

---
+++ src/com/lseg/Homework3/IfHomework.java
@@ -0,0 +1,18 @@
+package com.lseg.Homework3;
+
+public class IfHomework {
+    public static void main(String[] args) {
+
+        //1.  Write a function called convertToKilometersPerHour(double milesPerHour) that would return the
+        //converted value of speed from mile/hour into km/hour. The function definition should be:
+        //static double convertToKilometersPerHour(double milesPerHour);
+
+
+        static double convertToKilometersPerHour(double milesPerHour){
+        }
+
+    }
+}
+
+
+
