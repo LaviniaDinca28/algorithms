938b12e7c199dca32ca442f3833790dee3b7b03d
LaviniaDinca28 <40751786+LaviniaDinca28@users.noreply.github.com>
2020-09-03 09:49:42 +0300

    Homework3

--- src/com/lseg/Homework3/Ex4.java
+++ src/com/lseg/Homework3/Ex4.java
@@ -4,23 +4,27 @@ public class Ex4 {
     public static void main(String[] args) {
 
 
-        System.out.println(isCatPlaying(true, 26));
+        System.out.println(isCatPlaying(true, 45));
     }
 
     private static boolean isCatPlaying(boolean summer, int temperature){
-        boolean result = false;
-        if(summer = true & (temperature < 25 || temperature > 45)){
+        if (summer){
+            if (temperature >= 25 && temperature <= 45){
+                System.out.println("Cat is playing");
+                return true;
+            }
+            else {
+                System.out.println("Cat is not playing");
+                return false;
+            }
+        }
+        else if (temperature >= 25 && temperature <= 35){
+            System.out.println("Cat is playing");
+            return true;
+        }
+        else {
             System.out.println("Cat is not playing");
             return false;
         }
-        else if(summer = false & (temperature < 25 || temperature > 35)){
-            System.out.println("Cat is not playing");
-            return false;
-        }
-        else if (summer = false & (temperature > 25 || temperature <35)){
-            System.out.println("Cats are playing");
-        return true;
-        }
-        return result;
     }
 }
