9f453bcfe70897a7668b7e8854573a6a309de90b
LaviniaDinca28 <40751786+LaviniaDinca28@users.noreply.github.com>
2020-09-22 23:14:50 +0300

    Homework6

---
+++ src/com/lseg/Homework6/ComplexNumber.java
@@ -0,0 +1,20 @@
+package com.lseg.Homework6;
+
+public class ComplexNumber {
+
+ private double real;
+ private double imaginary;
+
+ //constructor
+    public ComplexNumber(double real, double imaginary){
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal(){return real;}
+    public double getImaginary(){return imaginary;}
+
+    public double add(double n1, double n2){
+        temp.real = n1.real + n2.real;
+    }
+}
