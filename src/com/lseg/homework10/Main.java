2a44847ca30d0a3cb908a4c5ac80df6fb97e0f88
LaviniaDinca28 <40751786+LaviniaDinca28@users.noreply.github.com>
2020-11-01 16:25:08 +0200

    Homework10

--- src/com/lseg/homework10/Main.java
+++ src/com/lseg/homework10/Main.java
@@ -15,10 +15,10 @@ public class Main {
         BankAccount bankAccount3 = new BankAccount("RO12345678912");
         BankAccount bankAccount4 = new BankAccount("RO12345678913");
 
-        Card card1 = new Card(LocalDate.of(2025,10,20), "Lavinia Dinca", 1234567);
-        Card card2 = new Card(LocalDate.of(2026,10,20), "Bastien Vizz", 1234568);
-        Card card3 = new Card(LocalDate.of(2027,10,20), "Camelia D", 1234577);
-        Card card4 = new Card(LocalDate.of(2019,10,20), "James C", 12345647);
+        Card card1 = new Card(LocalDate.of(2025, 10, 20), "Lavinia Dinca", 1234567);
+        Card card2 = new Card(LocalDate.of(2026, 10, 20), "Bastien Vizz", 1234568);
+        Card card3 = new Card(LocalDate.of(2027, 10, 20), "Camelia D", 1234577);
+        Card card4 = new Card(LocalDate.of(2019, 10, 20), "James C", 12345647);
 
         bankAccount1.addMoney(253.5);
         bankAccount2.addMoney(25);
@@ -40,12 +40,8 @@ public class Main {
         user3.addBankAccount(bankAccount3);
         user4.addBankAccount(bankAccount4);
 
-
-
-
-
-
-
+        PosService pos = new PosService();
+        
 
 
     }
--- src/com/lseg/homework10/PosService.java
+++ src/com/lseg/homework10/PosService.java
@@ -1,16 +1,37 @@
 package com.lseg.homework10;
 
+import java.time.LocalDate;
+import java.time.chrono.ChronoLocalDate;
+
 public class PosService {
 
     private BankAccount[] bankAccount = new BankAccount[1];
 
     public String pay(int amount, Card card) {
-        for(int i, i<=
+        for (int i = 0; i < bankAccount.length; i++) {
+            int cardNumber = bankAccount[i].getAttachedCardNumbers()[0];
+            if (card.getCardNumber() == cardNumber) {
+                System.out.printf("The card exists");
+                if (card.getExpirationDate().compareTo(LocalDate.now()) >= 0) {
+                    System.out.printf("The card is not expired");
+                    if (bankAccount[i].getBalance() >= amount) {
+                        bankAccount[i].withdrawMoney(amount);
+                        return "It withdrew the money" + amount;
+                    } else {
+                        return "There is not enough money";
+                    }
+                } else {
+                    return "The card is expired";
+                }
+            } else {
+                return "The card does not exist";
+            }
+        }
         return null;
     }
 
     public void addKnownBankAccount(BankAccount bankAccount) {
-     this.bankAccount[0] = bankAccount;
+        this.bankAccount[0] = bankAccount;
     }
 
     public BankAccount[] getBankAccount() {
