f3fbf2ffd36a0a7148d747fdeb0baddafad4d3a7
LaviniaDinca28 <40751786+LaviniaDinca28@users.noreply.github.com>
2020-11-01 14:14:45 +0200

    Homework10

--- src/com/lseg/homework10/BankAccount.java
+++ src/com/lseg/homework10/BankAccount.java
@@ -6,12 +6,31 @@ public class BankAccount {
     private double balance;
     private int[] attachedCardNumbers = new int[1];
 
-    public BankAccount(String ibanParameter){
+    public BankAccount(String ibanParameter) {
         this.iban = ibanParameter;
     }
 
-    public void addMoney (double amount){
+    public void addMoney(double amount) {
         this.balance += amount;
     }
 
+    public void withdrawMoney(double amount) {
+        this.balance = this.balance - amount;
+    }
+
+    public void attachCard(int cardNumber) {
+        this.attachedCardNumbers[0] = cardNumber;
+    }
+
+    public String getIban() {
+        return iban;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public int[] getAttachedCardNumbers() {
+        return attachedCardNumbers;
+    }
 }
--- src/com/lseg/homework10/Card.java
+++ src/com/lseg/homework10/Card.java
@@ -1,10 +1,29 @@
 package com.lseg.homework10;
 
+import java.util.Date;
+
 public class Card {
 
+    private Date expirationDate;
+    private String ownerName;
+    private int cardNumber;
 
+    public Card(Date expirationDate, String ownerName, int cardNumber) {
+        this.expirationDate = expirationDate;
+        this.ownerName = ownerName;
+        this.cardNumber = cardNumber;
+    }
 
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
 
+    public String getOwnerName() {
+        return ownerName;
+    }
 
+    public int getCardNumber() {
+        return cardNumber;
+    }
 }
 
