1e24bde4c01c43ab43f979d71eb41569afa41e01
LaviniaDinca28 <40751786+LaviniaDinca28@users.noreply.github.com>
2020-11-19 14:47:09 +0200

    Homework12

--- src/com/lseg/homework12/MatchingAnythingButaNewline.java
+++ src/com/lseg/homework12/MatchingAnythingButaNewline.java
@@ -11,7 +11,7 @@ public class MatchingAnythingButaNewline {
     public static void main(String[] args) {
 
         Tester tester = new Tester();
-        tester.check("..............."); //
+        tester.check("^...\\....\\....\\....$"); //
 
     }
 }
